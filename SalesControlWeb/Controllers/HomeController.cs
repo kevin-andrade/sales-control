d64f83d287e0d5f7cee9d156e23fbcae4a676b2b
Keven Andrade <nivekandrade@live.com>
2023-12-08 16:18:51 -0300

    Implementing grouping search

--- SalesControlWeb/Controllers/HomeController.cs
+++ SalesControlWeb/Controllers/HomeController.cs
@@ -1,47 +1,47 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using SalesControlWeb.Models;
 using SalesControlWeb.Models.ViewModels;
 
-namespace SalesControlWeb.Controllers;
-
-public class HomeController : Controller
+namespace SalesControlWeb.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-    public IActionResult Contact()
-    {
-        ViewData["Message"] = "Links";
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-        return View();
-    }
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Links";
 
-    public IActionResult About()
-    {
-        ViewData["Message"] = "Sales Control Web App from C#";
-        ViewData["Dev"] = "Keven Andrade";
+            return View();
+        }
 
-        return View();
-    }
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Sales Control Web App from C#";
+            ViewData["Dev"] = "Keven Andrade";
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
--- SalesControlWeb/Models/SalesRecord.cs
+++ SalesControlWeb/Models/SalesRecord.cs
@@ -6,6 +6,8 @@ namespace SalesControlWeb.Models
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:N2}")]
